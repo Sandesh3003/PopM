366d5284b0144df6fca86389584b566a5921b424
Sandesh Ratnawat <sandeshratnawat20320@acropolis.in>
2023-08-25 23:37:11 +0530

    Switching to github repos

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import './style.css'
+import Home from './views/home'
+
+const App = () => {
+
+  const ref= useRef(null);
+  
+
+  return (
+    <Router>
+      <div>
+        <Route component={Home} exact path="/" />
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
